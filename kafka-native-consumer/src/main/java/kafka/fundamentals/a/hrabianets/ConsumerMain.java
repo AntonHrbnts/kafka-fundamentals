4eaa375275471c79646cea20ec33146c4b7c2dc7
Anton Hrabianets <anton.hrabianets@gmail.com>
2024-08-21 12:12:39 +0200

    replication & partitioning

--- kafka-native-consumer/src/main/java/kafka/fundamentals/a/hrabianets/ConsumerMain.java
+++ kafka-native-consumer/src/main/java/kafka/fundamentals/a/hrabianets/ConsumerMain.java
@@ -3,6 +3,9 @@ package kafka.fundamentals.a.hrabianets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
+
+import org.apache.kafka.clients.admin.Admin;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -10,7 +13,15 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 public class ConsumerMain {
     public static void main(String[] args) {
+        Admin admin = Admin.create(getBootstrapProperties());
+
+        short evenReplicationFactor = 2;
+        short oddReplicationFactor = 3;
+        NewTopic odd = new NewTopic("odd", 2, oddReplicationFactor);
+        NewTopic even = new NewTopic("even", 3,evenReplicationFactor);
+        admin.createTopics(List.of(odd, even));
         KafkaConsumer consumer = getKafkaConsumer();
+
         consumer.subscribe(getTopics());
 
         System.out.println("Starting listener");
@@ -38,12 +49,18 @@ public class ConsumerMain {
 
     private static Properties getProperties() {
         Properties props = new Properties();
-        System.out.println("BOOTSTRAP_SERVERS_CONFIG: " + System.getenv("BOOTSTRAP_SERVERS_CONFIG"));
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv("BOOTSTRAP_SERVERS_CONFIG"));
+        props.putAll(getBootstrapProperties());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         System.out.println("GROUP_ID_CONFIG: " + System.getenv("GROUP_ID_CONFIG"));
         props.put(ConsumerConfig.GROUP_ID_CONFIG, System.getenv("GROUP_ID_CONFIG"));
+        return props;
+    }
+
+    private static Properties getBootstrapProperties(){
+        Properties props = new Properties();
+        System.out.println("BOOTSTRAP_SERVERS_CONFIG: " + System.getenv("BOOTSTRAP_SERVERS_CONFIG"));
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv("BOOTSTRAP_SERVERS_CONFIG"));
         return props;
     }
 }

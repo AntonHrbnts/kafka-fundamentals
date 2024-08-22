65b55cbdf8cfceb9b1a18375fc562fc4d41e59d4
a.hrabianets <anton.hrabianets@godel.shellenergy.co.uk>
2024-08-22 14:21:37 +0200

    consumer clean up

--- Makefile
+++ Makefile
@@ -12,9 +12,11 @@ build: maven_build consumer producer
 start:
 	cd docker && \
 	docker compose -f docker-compose.yml up -d && \
- 	docker compose exec broker-1 ./opt/kafka/bin/kafka-topics.sh --create --topic odd --partitions 2 --replication-factor 3 --bootstrap-server broker-1:19092,broker-2:19092,broker-3:19092 && \
+ 	docker compose exec broker-1 ./opt/kafka/bin/kafka-topics.sh --create --topic odd --partitions 3 --replication-factor 3 --bootstrap-server broker-1:19092,broker-2:19092,broker-3:19092 && \
  	docker compose exec broker-1 ./opt/kafka/bin/kafka-topics.sh --create --topic even --partitions 2 --replication-factor 3 --bootstrap-server broker-1:19092,broker-2:19092,broker-3:19092 && \
  	docker compose -f docker-compose.consumers.yml -f docker-compose.producers.yml up -d
 
 stop:
 	cd docker && docker compose -f docker-compose.producers.yml -f docker-compose.consumers.yml -f docker-compose.yml down
+
+restart: stop start
--- kafka-native-consumer/src/main/java/kafka/fundamentals/a/hrabianets/ConsumerMain.java
+++ kafka-native-consumer/src/main/java/kafka/fundamentals/a/hrabianets/ConsumerMain.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-import org.apache.kafka.clients.admin.Admin;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -17,27 +15,21 @@ public class ConsumerMain {
     public static void main(String[] args) {
 
         printSysVariables();
-        createTopic();
 
-        KafkaConsumer consumer = getKafkaConsumer();
-        consumer.subscribe(getTopics());
+        try (KafkaConsumer consumer = getKafkaConsumer()) {
+            consumer.subscribe(getTopics());
 
-        System.out.println("Starting listener");
-        while (true) {
-            ConsumerRecords<String, String> recs = consumer.poll(10);
-            if (recs.count() == 0) {
-            } else {
-                for (ConsumerRecord<String, String> rec : recs) {
-                    System.out.println(String.format("Recieved %s: %s", rec.key(), rec.value()));
+            System.out.println("Starting listener");
+            while (true) {
+                ConsumerRecords<String, String> recs = consumer.poll(10);
+                if (recs.count() == 0) {
+                } else {
+                    for (ConsumerRecord<String, String> rec : recs) {
+                        System.out.println(String.format("Recieved %s: %s", rec.key(), rec.value()));
+                    }
                 }
             }
         }
-
-    }
-
-    private static void createTopic() {
-        Admin admin = Admin.create(getBootstrapProperties());
-        admin.createTopics(getNewTopics());
     }
 
     private static void printSysVariables() {
@@ -45,15 +37,6 @@ public class ConsumerMain {
         for (String envName : env.keySet()) {
             System.out.format("%s=%s%n", envName, env.get(envName));
         }
-    }
-
-    private static List<NewTopic> getNewTopics() {
-        return Collections.singletonList(
-                new NewTopic(System.getenv("TOPIC_NAME"),
-                        Integer.parseInt(System.getenv("TOPIC_PARTITIONS")),
-                        Short.parseShort(System.getenv("TOPIC_REPLICATIONS"))
-                )
-        );
     }
 
     private static List<String> getTopics() {
@@ -68,18 +51,13 @@ public class ConsumerMain {
 
     private static Properties getProperties() {
         Properties props = new Properties();
-        props.putAll(getBootstrapProperties());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv("BOOTSTRAP_SERVERS_CONFIG"));
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringDeserializer");
         System.out.println("GROUP_ID_CONFIG: " + System.getenv("GROUP_ID_CONFIG"));
         props.put(ConsumerConfig.GROUP_ID_CONFIG, System.getenv("GROUP_ID_CONFIG"));
-        return props;
-    }
-
-    private static Properties getBootstrapProperties() {
-        Properties props = new Properties();
-        System.out.println("BOOTSTRAP_SERVERS_CONFIG: " + System.getenv("BOOTSTRAP_SERVERS_CONFIG"));
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv("BOOTSTRAP_SERVERS_CONFIG"));
         return props;
     }
 }
